42a59e6808eff86435d1daf1c583b197632cd588
Gerhardus <hardus.cronje@gmail.com>
2025-10-02 21:10:23 +0200

    feat: simplified setup and now works

--- src/main.rs
+++ src/main.rs
@@ -1,58 +1,50 @@
-use std::{error::Error, io::stdin, sync::{Arc, Mutex}, thread::{self, sleep}, time::Duration};
-
+use std::{
+    error::Error,
+    io::stdin,
+    thread::{self},
+    time::Duration,
+};
 
 fn main() -> Result<(), Box<dyn Error>> {
     let mut port = serialport::new("/dev/ttyUSB0", 115_200)
         .timeout(Duration::from_millis(100))
         .open()?;
 
-    let port_mux = Arc::new(Mutex::new(port));
-
-    let port_cp1 = port_mux.clone();
+    let mut port_cp1 = port.try_clone()?;
     let tr1 = thread::spawn(move || {
         loop {
-            let res = port_cp1.try_lock();
-            if let Ok(mut res) = res {
-                let mut buf: Vec<u8> = Vec::new();
-
-                let res = res.read_to_end(&mut buf);
-                if let Ok(x) = res {
-                    if x > 0 {
-                        println!("Msg: {}", String::from_utf8_lossy(&buf));
+            let mut buffer: [u8; 1] = [0; 1];
+            match port_cp1.read(&mut buffer) {
+                Ok(bytes) => {
+                    if bytes == 1 {
+                        print!("{}", String::from_utf8_lossy(&buffer));
                     }
                 }
+                Err(ref e) if e.kind() == std::io::ErrorKind::TimedOut => (),
+                Err(e) => eprintln!("{:?}", e),
             }
-            sleep(Duration::from_millis(10));
         }
     });
 
     loop {
         let mut input = String::new();
         let _bytes_read = stdin().read_line(&mut input)?;
-        println!("{input}");
-        if let Ok(mut port) = port_mux.try_lock() {
-            let res = port.write_all((input.trim().to_owned() + "\n\r").as_bytes());
-            port.flush()?;
-            match res {
-                Ok(x) => {
-                    // if x > 0 {
-                    //     println!("Bytes written: {}", x);
-                    // }
-                    x
-                },
-                Err(e) => {
-                    println!("{e}");
-                    break;
-                },
+        let res = port.write((input.trim().to_owned() + "\r\n").as_bytes());
+        port.flush()?;
+        match res {
+            Ok(x) => {
+                println!("Bytes written: {x}");
             }
-
-        };
+            Err(e) => {
+                println!("{e}");
+                break;
+            }
+        }
     }
 
-    tr1.join()
-        .map_err(|_e| std::io::Error::new(
-            std::io::ErrorKind::Other, format!("Something went wrong."))
-        )?;
+    tr1.join().map_err(|_e| {
+        std::io::Error::new(std::io::ErrorKind::Other, format!("Something went wrong."))
+    })?;
 
     Ok(())
 }
