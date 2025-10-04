4913ca1cfa635becbf53881d53dab262a3b8adc8
Gerhardus <hardus.cronje@gmail.com>
2025-10-04 16:22:48 +0200

    feat: event loop working

--- src/lib.rs
+++ src/lib.rs
@@ -1,20 +1,26 @@
-use std::{collections::{HashMap, VecDeque}, sync::{mpsc::{channel, Receiver, Sender}, Arc, Mutex}};
+use std::{collections::HashMap, sync::{mpsc::{channel, Receiver, Sender}, Arc, Mutex}};
 
-struct Event {
+pub struct Event {
     name: String,
     data: String,
 }
 
 impl Event {
-    fn new(name: String, data: String) -> Self {
+    pub fn new(name: String, data: String) -> Self {
         Event { name, data }
     }
 }
 
-struct EventLoop {
+impl From<Event> for String {
+    fn from(value: Event) -> Self {
+        format!("{}: {}", value.name, value.data)
+    }
+}
+
+pub struct EventLoop {
     receiver: Receiver<Event>,
-    sender: Sender<Event>,
-    handlers: Arc<Mutex<HashMap<String, Box<dyn Fn(Event) -> ()>>>>
+    pub sender: Sender<Event>,
+    handlers: Arc<Mutex<HashMap<String, Box<dyn FnMut(Event) -> ()>>>>
 }
 
 impl EventLoop {
@@ -28,8 +34,8 @@ impl EventLoop {
             let msg = self.receiver.recv();
             match msg {
                 Ok(msg) => {
-                    if let Ok(handlers) = self.handlers.try_lock() {
-                        let event = handlers.get(&msg.name);
+                    if let Ok(mut handlers) = self.handlers.try_lock() {
+                        let event = handlers.get_mut(&msg.name);
                         if let Some(ex) = event {
                             ex(msg);
                         }
@@ -42,9 +48,9 @@ impl EventLoop {
         }
     }
 
-    pub fn on<F>(&mut self, name: String, func: F)
+    pub fn on<F>(&self, name: String, func: F)
     where
-        F : Fn(Event) -> () + 'static
+        F : FnMut(Event) -> () + 'static
     {
         if let Ok(mut handlers) = self.handlers.lock() {
             handlers.insert(
@@ -52,12 +58,5 @@ impl EventLoop {
                 Box::new(func)
             );
         }
-    }
-
-    pub fn dispatch(&mut self, event: Event) -> Result<(), String> {
-        self.sender.send(event)
-            .map_err(|_| "failed to lock mux".to_owned())?;
-
-        Ok(())
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
 use std::{
-    error::Error, io::stdin, thread::{self}, time::Duration
+    error::Error, io::{stdin, ErrorKind}, thread::{self}, time::Duration
 };
 
+use at_commander::{Event, EventLoop};
 use clap::{command, Parser};
 
 /// Simple program to communicate AT commands with the ESP-01 module.
@@ -35,11 +36,16 @@ fn parse_bytes(input: &str, radix: u8) -> Result<Vec<u8>, String> {
 fn main() -> Result<(), Box<dyn Error>> {
     let args = Args::parse();
 
+    let event_loop = EventLoop::new();
+
     let mut port = serialport::new(&args.port, args.baud_rate)
         .timeout(Duration::from_millis(100))
         .open()?;
 
     let mut port_cp1 = port.try_clone()?;
+    let mut port_cp2 = port.try_clone()?;
+
+    // READING TASK
     let tr1 = thread::spawn(move || {
         loop {
             let mut buffer: [u8; 1] = [0; 1];
@@ -58,88 +64,125 @@ fn main() -> Result<(), Box<dyn Error>> {
         }
     });
 
-    loop {
-        let mut input = String::new();
-        let _bytes_read_to_input = stdin().read_line(&mut input)?;
+    // USER INPUT TASK
+    let tr2_sender = event_loop.sender.clone();
+    let tr2 = thread::spawn(move || -> Result<(), ErrorKind> {
+        loop {
+            let mut input = String::new();
+            let _bytes_read_to_input = stdin().read_line(&mut input).map_err(|_| ErrorKind::Other)?;
 
-        let mut trimmed_input = input.trim().to_owned();
+            let mut trimmed_input = input.trim().to_owned();
 
-        // Send raw bytes if string ends with ~
-        let payload = if trimmed_input.ends_with("~") {
-            trimmed_input.pop();
-            let bytes_vec = parse_bytes(&trimmed_input, args.radix_input_buffer)?;
-            bytes_vec
-        // Send MQTT message
-        } else if trimmed_input.starts_with("con") {
-            // Client name always client 1.
-            // Byte #	Value	Field	Description
-            // 1	10	Fixed header	CONNECT packet, flags=0
-            // 2	13	Remaining Length	19 bytes
-            // 3-4	00 04	Protocol Name Length	4 bytes
-            // 5-8	4D 51 54 54	Protocol Name	"MQTT"
-            // 9	04	Protocol Level	MQTT 3.1.1
-            // 10	02	Connect Flags	CleanSession=1
-            // 11-12	00 3C	Keep Alive	60 seconds
-            // 13-14	00 07	Client ID length	7 bytes
-            // 15-21	63 6C 69 65 6E 74 31	Client ID	"client1"
-            let msg = "\x10\x13\x00\x04MQTT\x04\x02\x00\x3C\x00\x07client1".as_bytes().to_vec();
-            msg
-        } else if trimmed_input.starts_with("msg") {
-            let args: Vec<String> = trimmed_input.split(":").map(|x| x.to_owned()).collect();
+            // Send raw bytes if string ends with ~
+            let payload = if trimmed_input.ends_with("~") {
+                trimmed_input.pop();
+                let bytes_vec = parse_bytes(&trimmed_input, args.radix_input_buffer).map_err(|_| ErrorKind::Other)?;
+                bytes_vec
+            // Send MQTT message
+            } else if trimmed_input.starts_with("con") {
+                // Client name always client 1.
+                // Byte #	Value	Field	Description
+                // 1	10	Fixed header	CONNECT packet, flags=0
+                // 2	13	Remaining Length	19 bytes
+                // 3-4	00 04	Protocol Name Length	4 bytes
+                // 5-8	4D 51 54 54	Protocol Name	"MQTT"
+                // 9	04	Protocol Level	MQTT 3.1.1
+                // 10	02	Connect Flags	CleanSession=1
+                // 11-12	00 3C	Keep Alive	60 seconds
+                // 13-14	00 07	Client ID length	7 bytes
+                // 15-21	63 6C 69 65 6E 74 31	Client ID	"client1"
+                let msg = "\x10\x13\x00\x04MQTT\x04\x02\x00\x3C\x00\x07client1".as_bytes().to_vec();
+                msg
+            } else if trimmed_input.starts_with("msg") {
+                let args: Vec<String> = trimmed_input.split(":").map(|x| x.to_owned()).collect();
 
-            let topic = match args.get(1) {
-                Some(x) => x.to_owned(),
-                None => "/home".to_owned(),
+                let topic = match args.get(1) {
+                    Some(x) => x.to_owned(),
+                    None => "/home".to_owned(),
+                };
+
+                let message = match args.get(2) {
+                    Some(x) => x.to_owned(),
+                    None => "HEY".to_owned(),
+                };
+
+                let topic_len = topic.len();
+                let topic_and_message_len = topic_len + message.len();
+
+                // Byte #	Value	Field	Description
+                // 1	30	Fixed header	PUBLISH packet, QoS=0, DUP=0, Retain=0
+                // 2	12	Remaining Length	18 bytes (variable header + payload)
+                // 3-4	00 0B	Topic Name Length	11 bytes
+                // 5-15	2F 74 65 73 74 2F 74 6F 70 69 63	Topic Name = "/test/topic"	
+                // 16-20	68 65 6C 6C 6F	Payload = "hello"
+                let mut buff: Vec<u8> = vec![
+                    0x30,
+                    topic_and_message_len.try_into().unwrap_or(0xFF),
+                    if topic_len > 0xFF { (topic_len - 0xFF).try_into().unwrap_or(255) } else { 0x00 },
+                    topic_len.try_into().unwrap_or(0xFF),
+                ];
+
+                let mut topic_msg_u8 = (topic + &message).into_bytes();
+
+                buff.append(&mut topic_msg_u8);
+                buff
+            } else if trimmed_input == "test" {
+                tr2_sender.send(
+                    Event::new("connect".to_owned(), "asdasd".to_owned())
+                );
+                "\r\n".as_bytes().to_vec()
+
+            } else {
+                (trimmed_input.to_owned() + "\r\n").as_bytes().to_vec()
             };
 
-            let message = match args.get(2) {
-                Some(x) => x.to_owned(),
-                None => "HEY".to_owned(),
-            };
+            println!("Sending {} bytes", payload.len());
 
-            let topic_len = topic.len();
-            let topic_and_message_len = topic_len + message.len();
+            let res = port.write(payload.as_slice());
+            port.flush().map_err(|_| ErrorKind::Other)?;
 
-            // Byte #	Value	Field	Description
-            // 1	30	Fixed header	PUBLISH packet, QoS=0, DUP=0, Retain=0
-            // 2	12	Remaining Length	18 bytes (variable header + payload)
-            // 3-4	00 0B	Topic Name Length	11 bytes
-            // 5-15	2F 74 65 73 74 2F 74 6F 70 69 63	Topic Name = "/test/topic"	
-            // 16-20	68 65 6C 6C 6F	Payload = "hello"
-            let mut buff: Vec<u8> = vec![
-                0x30,
-                topic_and_message_len.try_into().unwrap_or(0xFF),
-                if topic_len > 0xFF { (topic_len - 0xFF).try_into().unwrap_or(255) } else { 0x00 },
-                topic_len.try_into().unwrap_or(0xFF),
-            ];
-
-            let mut topic_msg_u8 = (topic + &message).into_bytes();
-
-            buff.append(&mut topic_msg_u8);
-            buff
-        } else {
-            (trimmed_input.to_owned() + "\r\n").as_bytes().to_vec()
-        };
-
-        println!("Sending {} bytes", payload.len());
-
-        let res = port.write(payload.as_slice());
-        port.flush()?;
-
-        match res {
-            Ok(x) => {
-                println!("Bytes written: {x}");
-            }
-            Err(e) => {
-                println!("{e}");
-                break;
+            match res {
+                Ok(x) => {
+                    println!("Bytes written: {x}");
+                }
+                Err(e) => {
+                    println!("{e}");
+                    break;
+                }
             }
         }
-    }
+        Ok(())
+    });
+
+    let connect_responsed_sender = event_loop.sender.clone();
+    event_loop.on("connect".to_owned(), move |e| {
+        // port_cp2.write("".as_bytes());
+        println!("CONNECT EVENT RECEIVED");
+        connect_responsed_sender.send(
+            Event::new("connected".to_owned(), "".to_owned())
+        );
+    });
+
+    event_loop.on("connected".to_owned(), |e| {
+        println!("CONNECTED EVENT RECEIVED");
+    });
+
+    event_loop.start();
 
     tr1.join().map_err(|_e| {
         std::io::Error::new(std::io::ErrorKind::Other, format!("Something went wrong."))
     })?;
+    let res = tr2.join().map_err(|_e| {
+        std::io::Error::new(std::io::ErrorKind::Other, format!("Something went wrong."))
+    })?;
+    match res {
+        Ok(_) => {
+            println!("Returned without errors");
+        },
+        Err(_e) => {
+            println!("Something went wrong somewhere, if your code were better, we would know where");
+        },
+    }
 
     Ok(())
 }
